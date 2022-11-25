a9027b1893e426e9d58de457a5ce78064ab150c0
Eddy Filip <eddy.filip@agilebits.com>
2022-11-25 17:02:31 +0100

    Adjust CLI version used

--- src/test/java/com/onepassword/jenkins/plugins/util/TestConstants.java
+++ src/test/java/com/onepassword/jenkins/plugins/util/TestConstants.java
@@ -10,7 +10,7 @@ public class TestConstants {
     public static final String TEST_FAILURE = "Strings are not equal.";
 
     public static final String TEST_CONNECT_HOST = "http://localhost:8080";
-    public static final String OP_CLI_URL = "https://cache.agilebits.com/dist/1P/op2/pkg/v2.8.0-beta.05/op_linux_amd64_v2.8.0-beta.05.zip";
+    public static final String OP_CLI_URL = "https://cache.agilebits.com/dist/1P/op2/pkg/v2.8.0-beta.06/op_linux_amd64_v2.8.0-beta.06.zip";
     public static final String TEST_SECRET = "RGVhciBzZWN1cml0eSByZXNlYXJjaGVyLCB0aGlzIGlzIGp1c3QgYSBkdW1teSBzZWNyZXQuIFBsZWFzZSBkb24ndCByZXBvcnQgaXQu";
 
     public static String readFile(String path, Charset encoding)
