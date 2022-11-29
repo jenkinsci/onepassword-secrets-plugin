7a0ff9fbf90bee48931e39b8b38826485a3bd0c3
Eduard Filip <eddy.filip@agilebits.com>
2022-11-29 09:29:55 +0100

    Enable to run integrations tests for this plugin (#1)
    This brings the following key changes:
    - Run the written integration tests in a separate GitHub Action pipeline
    - Skip the integration tests from being executed in the Jenkins instance
    
    This has been done for the following reasons:
    - It seems too challenging to run in the Jenkins instance a docker compose to run the Connect instance needed for the tests, as well as pass the right environment variables needed for them.
    - We want to show that our plugin works as expected, which is covered by the integration tests written.

--- src/test/java/com/onepassword/jenkins/plugins/util/TestConstants.java
+++ src/test/java/com/onepassword/jenkins/plugins/util/TestConstants.java
@@ -9,8 +9,8 @@ public class TestConstants {
     public static final String TEST_SUCCESS = "Strings are equal.";
     public static final String TEST_FAILURE = "Strings are not equal.";
 
-    public static final String TEST_CONNECT_HOST = "https://9ace-2001-1c00-b00-600-dc16-4ccd-d24d-4c9d.eu.ngrok.io";
-    public static final String OP_CLI_URL = "https://cache.agilebits.com/dist/1P/op2/pkg/v2.8.0-beta.05/op_linux_amd64_v2.8.0-beta.05.zip";
+    public static final String TEST_CONNECT_HOST = "http://localhost:8080";
+    public static final String OP_CLI_URL = "https://cache.agilebits.com/dist/1P/op2/pkg/v2.8.0-beta.09/op_linux_amd64_v2.8.0-beta.09.zip";
     public static final String TEST_SECRET = "RGVhciBzZWN1cml0eSByZXNlYXJjaGVyLCB0aGlzIGlzIGp1c3QgYSBkdW1teSBzZWNyZXQuIFBsZWFzZSBkb24ndCByZXBvcnQgaXQu";
 
     public static String readFile(String path, Charset encoding)
